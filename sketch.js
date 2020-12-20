d5f888b7ae8796ac967e95d3127d063152f3bb31
Devank18 <67155685+Devank18@users.noreply.github.com>
2020-12-20 09:21:46 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,21 +6,19 @@ var foodobject
 var Feedtime
 var Lastfeed
 
-
-function preload()
-
-{
+function preload(){
   dogimg1 = loadImage("images/dogImg.png")
   dogimg2 = loadImage("images/dogImg1.png")
 }	
 
-function setup() 
-{
+function setup() {
 	createCanvas(1000, 500);
+	
   database = firebase.database();
   console.log(database);
  
   foodobject=new Food()
+  
   dog = createSprite(550,250,10,10);
   dog.addImage(dogimg1)
   dog.scale=0.2
@@ -32,58 +30,41 @@ function setup()
   feed.position(500,15)
   feed.mousePressed(FeedDog)
   
-  ///
   add = createButton("Add Food")
   add.position(400,15)
   add.mousePressed(AddFood)
-
 } 
 
 function draw(){
   background(46,139,87);
-
     foodobject.display()
       
+	
 drawSprites();
-  
  fill(255,255,254);
  textSize(15);
-
 drawSprites();
 }
-
-
-
-
-
-function readPosition(data)
-{
+function readPosition(data){
   position = data.val();
   foodobject.updateFoodStock(position)
 }
-
-
-
-function writePosition(nazo)
-{
-  if(nazo>0)
-  {
+function writePosition(nazo){
+  if(nazo>0){
     nazo=nazo-1
   }
-  else
-  {
+  else{
     nazo=0
   }
-     database.ref('/').set({
-      'Food': nazo
+  
+   database.ref('/').set({
+  'Food': nazo
   })
 
 }
 
-function AddFood()
-{
+function AddFood(){
     position++
-    
     database.ref('/').update({
     Food:position
     })
